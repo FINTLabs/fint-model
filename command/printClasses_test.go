7bb504df037f6ce02904c7c0fffbe5e6585d9a86
Frode Sjovatsen <frode@sjovatsen.net>
2017-06-16 13:52:05 +0200

    initial commit

---
+++ command/printClasses_test.go
@@ -0,0 +1,7 @@
+package command
+
+import "testing"
+
+func TestCmdPrintClasses(t *testing.T) {
+	// Write your code here
+}
---
+++ version.go
@@ -0,0 +1,4 @@
+package main
+
+const Name string = "fint-model"
+const Version string = "0.1.0"
