cfc448e9edc8ea8826e884c05c1312377a160ce8
Asgeir Nilsen <asgeir@fintprosjektet.no>
2017-08-25 17:41:14 +0200

    Fixed compilation error from previous commit.  Also fixed .gitignore and improved Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM golang:latest
 
-WORKDIR /go/src/app/vendor/github.com/FINTprosjektet/fint-model
-COPY . .
-
 RUN go-wrapper download github.com/mitchellh/gox
 RUN go-wrapper install github.com/mitchellh/gox
+
+WORKDIR /go/src/app/vendor/github.com/FINTprosjektet/fint-model
+
+COPY . .
+
 RUN go-wrapper download
-RUN gox -output="./build/{{.OS}}/{{.Dir}}" -rebuild -osarch="windows/amd64"
-RUN curl --upload-file build/windows/fint-model.exe https://transfer.sh/fint-model.exe
+RUN gox -output="./build/{{.Dir}}-{{.OS}}" -rebuild -osarch="windows/amd64"
+RUN curl --upload-file build/fint-model-windows.exe https://transfer.sh/fint-model-windows.exe
---
+++ generate/java/java_class_tpl.go
@@ -0,0 +1,41 @@
+package java
+
+const CLASS_TEMPLATE = `package {{ .Package }};
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+{{- if .Imports }}
+{{ range $i := .Imports }}
+import {{ $i }};
+{{- end -}}
+{{ end }}
+
+@Data
+{{ if .Extends -}}
+@EqualsAndHashCode(callSuper=false)
+{{ else -}}
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+{{ end -}}
+public {{- if .Abstract }} abstract {{- end }} class {{ .Name }} {{ if .Extends -}} extends {{ .Extends }} {{ end -}} {
+
+{{- if .Relations }}
+	{{ $c := sub (len .Relations) 1 -}}
+	public enum Relasjonsnavn {
+		{{- range $i, $rel := .Relations }}
+			{{ $rel }}{{ if ne $i $c }},{{ end -}}
+		{{ end }}
+	}
+{{ end -}}
+{{ if .Attributes }}
+	{{ range $att := .Attributes -}}
+		private {{ javaType $att.Type}} {{ $att.Name }};
+	{{ end -}}
+{{ end -}}
+}
+
+`
