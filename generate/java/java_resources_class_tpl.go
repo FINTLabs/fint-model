15a8138e03533ba4ec5951b087bbf673b55989d0
Asgeir Nilsen <asgeir@fintprosjektet.no>
2018-08-13 13:26:23 +0200

    Convenience constructor with contents. Deprecated type reference.

--- generate/java/java_resources_class_tpl.go
+++ generate/java/java_resources_class_tpl.go
@@ -4,6 +4,7 @@ const RESOURCES_CLASS_TEMPLATE = `// Built from tag {{ .GitTag }}
 
 package {{ resourcePkg .Package }};
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.util.List;
@@ -12,6 +13,12 @@ import no.fint.model.resource.AbstractCollectionResources;
 
 public class {{ .Name }}Resources extends AbstractCollectionResources<{{.Name}}Resource> {
 
+    public {{.Name}}Resources(Collection<{{.Name}}Resource> input) {
+        super(input);
+    }
+
+    @JsonIgnore
+    @Deprecated
     @Override
     public TypeReference<List<{{.Name}}Resource>> getTypeReference() {
         return new TypeReference<List<{{.Name}}Resource>>() {};
