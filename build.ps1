641639122fc8eb055e9808b623e0f76514295dcb
Asgeir Nilsen <asgeir@fintlabs.no>
2020-08-26 15:38:56 +0200

    Update build.ps1
    Merge mishap

--- build.ps1
+++ build.ps1
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 docker build -t fint-model --build-arg VERSION=0.$(Get-Date -Format yyMMdd.HHmm) .
-=======
-docker build -t fint-model --build-arg VERSION=0.$(Get-Date -Format yyMMdd.HHmm) .
->>>>>>> master
