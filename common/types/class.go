f9d7b2bb796b41477c5714df33071dba1a792d33
Hknots <hknots99@gmail.com>
2024-02-19 02:18:33 +0100

    add ExtendsIdentifiable

--- common/types/class.go
+++ common/types/class.go
@@ -1,22 +1,23 @@
 package types
 
 type Class struct {
-	Name             string
-	Abstract         bool
-	Deprecated       bool
-	Extends          string
-	Package          string
-	Imports          []string
-	Namespace        string
-	Using            []string
-	Documentation    string
-	Attributes       []Attribute
-	Relations        []Association
-	ExtendsRelations bool
-	Resources        []Attribute
-	Resource         bool
-	ExtendsResource  bool
-	Identifiable     bool
-	GitTag           string
-	Stereotype       string
+	Name                string
+	Abstract            bool
+	Deprecated          bool
+	Extends             string
+	Package             string
+	Imports             []string
+	Namespace           string
+	Using               []string
+	Documentation       string
+	Attributes          []Attribute
+	Relations           []Association
+	ExtendsRelations    bool
+	Resources           []Attribute
+	Resource            bool
+	ExtendsResource     bool
+	Identifiable        bool
+	GitTag              string
+	Stereotype          string
+	ExtendsIdentifiable bool
 }
