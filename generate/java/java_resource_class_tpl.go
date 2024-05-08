bb9648f236fae57c1391bedd077185f4acdcfc10
Hknots <hknots99@gmail.com>
2024-05-08 21:05:52 +0200

    make use of resourcePackageRename

--- generate/java/java_resource_class_tpl.go
+++ generate/java/java_resource_class_tpl.go
@@ -19,7 +19,7 @@ import javax.validation.constraints.*;
 
 import no.fint.model.felles.kompleksedatatyper.Identifikator;
 import no.fint.model.resource.FintLinks;
-import no.fint.model.{{ resourceRename (javaType .Stereotype) }};
+import no.fint.model.{{ resourcePackageRename (javaType .Stereotype) }};
 import no.fint.model.resource.Link;
 import no.fint.model.FintIdentifikator;
 
