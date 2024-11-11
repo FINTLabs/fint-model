129f164cc70a34cf094790e09120263117ee89eb
Hknots <hknots99@gmail.com>
2024-11-12 00:13:52 +0100

    add Collections import

--- generate/java/java_class_tpl.go
+++ generate/java/java_class_tpl.go
@@ -10,6 +10,7 @@ import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import lombok.Getter;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
