1e5fe1223232da84a823d7f8693ed2e1bd7a5c3c
Ole Anders Eidjord <26850+oleanders@users.noreply.github.com>
2026-01-13 15:30:20 +0100

    Change to novari package name

--- common/config/constants.go
+++ common/config/constants.go
@@ -1,6 +1,6 @@
 package config
 
-const JAVA_PACKAGE_BASE = "no.fint.model"
+const JAVA_PACKAGE_BASE = "no.novari.fint.model"
 const NET_NAMESPACE_BASE = "FINT.Model"
 const DEFAULT_TAG = "latest"
 const JAVA_BASE_PATH = "java"
